9f7aea450263c048767efb62d928a4a29c5ca46f
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 20:30:49 +0530

    ADDED: As a gambler , would start with a stake of 100 every day and bet 1 every game

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class GamblingSimulation {
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulation Problem");
+        int stakeOfEveryDay = 100;
+        int betEveryGame = 1;
     }
 }
