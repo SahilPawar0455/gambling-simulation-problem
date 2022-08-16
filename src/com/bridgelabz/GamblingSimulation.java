74b49014a7824f782a20932c92958bd57dae4834
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 20:45:40 +0530

    ADDED: as a gambler make 1 bet so eithe win or loss 1 bit

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,9 +1,20 @@
 package com.bridgelabz;
 
 public class GamblingSimulation {
+    public static int stakeOfEveryDay = 100;
+    public static int betEveryGame = 1;
+    public static int win = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulation Problem");
-        int stakeOfEveryDay = 100;
-        int betEveryGame = 1;
+        int bit = (int) (Math.random() * (2) + 1);
+        if (bit == win) {
+            System.out.println("You are win");
+            stakeOfEveryDay++;
+        } else {
+            System.out.println("You are Loss");
+            stakeOfEveryDay--;
+        }
+        System.out.println("Now your stake is " + stakeOfEveryDay);
     }
 }
