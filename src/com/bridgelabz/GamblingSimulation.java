11e8f853950eaefea3dbb53e037908a5fba58f5c
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 20:57:57 +0530

    Modify: craete the method in problem then execute the problem

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -2,11 +2,15 @@ package com.bridgelabz;
 
 public class GamblingSimulation {
     public static int stakeOfEveryDay = 100;
-    public static int betEveryGame = 1;
-    public static int win = 1;
+    public static final int betEveryGame = 1;
+    public static final int win = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulation Problem");
+        playGame();
+    }
+
+    public static void playGame() {
         int bit = (int) (Math.random() * (2) + 1);
         if (bit == win) {
             System.out.println("You are win");
