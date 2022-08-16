15c669e7f31471fdfbee9a603799114595530445
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 21:14:47 +0530

    ADDED: as a calculate gamble if won or loss 50% of the stake

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class GamblingSimulation {
     public static int stakeOfEveryDay = 100;
-    public static final int betEveryGame = 1;
+    public static final int Bet_Every_Game = 1;
     public static final int win = 1;
 
     public static void main(String[] args) {
@@ -11,14 +11,21 @@ public class GamblingSimulation {
     }
 
     public static void playGame() {
-        int bit = (int) (Math.random() * (2) + 1);
-        if (bit == win) {
-            System.out.println("You are win");
-            stakeOfEveryDay++;
-        } else {
-            System.out.println("You are Loss");
-            stakeOfEveryDay--;
+        while (stakeOfEveryDay > 50 && stakeOfEveryDay < 150) {
+            int bit = (int) (Math.random() * (2) + 1);
+            if (bit == win) {
+                System.out.println("You are win");
+                stakeOfEveryDay++;
+            } else {
+                System.out.println("You are Loss");
+                stakeOfEveryDay--;
+            }
+            System.out.println("Now your stake is " + stakeOfEveryDay);
         }
-        System.out.println("Now your stake is " + stakeOfEveryDay);
+        if (stakeOfEveryDay == 50){
+            System.out.println("You have loss 50% stake");
+        }else {
+            System.out.println("You Have Win 50% stake");
+        }
     }
 }
