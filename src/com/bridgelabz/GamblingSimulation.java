8df4f8f2f5324e48cb81aa230754ba035382aebc
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-17 00:18:29 +0530

    ADDED: If won would like to continue playing next month or stop gambling

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,22 +1,39 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class GamblingSimulation {
     public static int stakeOfEveryDay = 100;
     public static int winDays = 0;
     public static int lossDays = 0;
+    public static boolean condition = true;
     public static final int DAYS = 20;
     public static final int WIN = 1;
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulation Problem");
         playGame();
-        GamblingSimulation gamblingSimulation = new GamblingSimulation();      // creating the object
-        gamblingSimulation.winLossDays();
+        nextMonth();
     }
 
-    public void winLossDays() {
-        System.out.println("luckiest day in this month is = "+winDays);
-        System.out.println("unluckiest day in this month is = "+lossDays);
+    public static void nextMonth() {
+        while (condition) {
+            System.out.println("choose the option \n1. Enter the 1 for play Again \n2. Enter the 2 for Exit");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Game is start Again");
+                    playGame();
+                    break;
+                case 2:
+                    System.out.println("Game is Over ");
+                    condition = false;
+                    break;
+                default:
+                    System.out.println("Please Enter the valid Number");
+            }
+        }
     }
 
     public static void playGame() {
@@ -46,5 +63,12 @@ public class GamblingSimulation {
                 lossDays++;
             }
         }
+        GamblingSimulation gamblingSimulation = new GamblingSimulation();      // creating the object
+        gamblingSimulation.winLossDays();
+    }
+
+    public void winLossDays() {
+        System.out.println("luckiest day in this month is = " + winDays);
+        System.out.println("unluckiest day in this month is = " + lossDays);
     }
 }
