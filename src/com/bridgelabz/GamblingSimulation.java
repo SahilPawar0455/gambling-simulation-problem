aeb726a4bd31ce26ff5dd30b5a7801be1e541738
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 21:59:01 +0530

    ADDED: Each month would like to know the days won and loss and by how much

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -2,13 +2,21 @@ package com.bridgelabz;
 
 public class GamblingSimulation {
     public static int stakeOfEveryDay = 100;
+    public static int winDays = 0;
+    public static int lossDays = 0;
     public static final int Bet_Every_Game = 1;
     public static final int Days = 20;
-    public static final int win = 1;
+    public static final int Win = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulation Problem");
         playGame();
+        GamblingSimulation gamblingSimulation = new GamblingSimulation();      // creating the object
+        gamblingSimulation.winLossDays();
+    }
+    public void winLossDays(){
+        System.out.println(winDays+" days win the game in this month");
+        System.out.println(lossDays+" days loss the game in this month");
     }
 
     public static void playGame() {
@@ -16,7 +24,7 @@ public class GamblingSimulation {
             int handCash = stakeOfEveryDay;
             while (handCash > 50 && handCash < 150) {
                 int bit = (int) (Math.random() * (2) + 1);
-                if (bit == win) {
+                if (bit == Win) {
                     System.out.println("You are win");
                     handCash++;
                 } else {
@@ -27,8 +35,10 @@ public class GamblingSimulation {
             }
             if (handCash == 50) {
                 System.out.println("You have loss 50% stake");
+                winDays++;
             } else {
                 System.out.println("You Have Win 50% stake");
+                lossDays++;
             }
         }
     }
